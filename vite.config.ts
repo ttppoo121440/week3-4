de14aa9d64956b1497a6c3bc46b4fbd5446949bf
Jethro <ttppoo12144@gmail.com>
2024-07-20 01:39:34 +1000

    修正 開發環境

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,20 @@
-import { defineConfig, UserConfig } from "vite";
+import { defineConfig, loadEnv, UserConfig, ConfigEnv } from "vite";
+import { resolve } from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
+import liveReload from "vite-plugin-live-reload";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { fileURLToPath } from "node:url";
 import path from "node:path";
 import { globSync } from "glob";
-import liveReload from "vite-plugin-live-reload";
 
-interface ServerOptions {
-  historyApiFallback?: boolean;
-}
 function moveOutputPlugin() {
   return {
     name: "move-output",
-    enforce: "post",
-    apply: "build",
-    async generateBundle(options, bundle) {
+    enforce: "post" as const,
+    apply: "build" as const,
+    async generateBundle(options: any, bundle: any) {
       for (const fileName in bundle) {
         if (fileName.startsWith("src/pages/")) {
           const newFileName = fileName.slice("src/pages/".length);
@@ -26,43 +25,51 @@ function moveOutputPlugin() {
   };
 }
 
-export default defineConfig({
-  base: import.meta.env.VITE_BASE_URL || "/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),
-        ...Object.fromEntries(
-          globSync("src/pages/**/*.html").map((file) => [
-            path.relative(
-              "src/pages",
-              file.slice(0, file.length - path.extname(file).length),
-            ),
-            file,
-          ]),
-        ),
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: mode === "development" ? "/" : env.VITE_BASE_URL || "/",
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "src/pages/index.html"),
+          ...Object.fromEntries(
+            globSync("src/pages/**/*.html").map((file) => [
+              path.relative(
+                "src/pages",
+                file.slice(0, file.length - path.extname(file).length),
+              ),
+              fileURLToPath(new URL(file, import.meta.url)),
+            ]),
+          ),
+        },
+      },
+      outDir: "dist",
+    },
+    server: {
+      port: 3000,
+      open: "/src/pages/index.html",
+      fs: {
+        allow: [resolve(__dirname, "src"), resolve(__dirname, "src/pages")],
       },
     },
-    outDir: "dist",
-  },
-  server: {
-    historyApiFallback: true,
-  } as ServerOptions,
-  plugins: [
-    tsconfigPaths(),
-    liveReload([
-      "./src/layout/**/*.ejs",
-      "./src/pages/**/*.ejs",
-      "./src/pages/**/*.html",
-    ]),
-    ViteEjsPlugin({
-      base: import.meta.env.VITE_BASE_URL || "/",
-    }),
-    moveOutputPlugin(),
-  ],
-  css: {
-    postcss: {
-      plugins: [tailwindcss(), autoprefixer],
+    plugins: [
+      tsconfigPaths(),
+      liveReload([
+        "./src/layout/**/*.ejs",
+        "./src/pages/**/*.ejs",
+        "./src/pages/**/*.html",
+      ]),
+      ViteEjsPlugin({
+        base: mode === "development" ? "/src/pages/" : env.VITE_BASE_URL || "/",
+      }),
+      moveOutputPlugin(),
+    ],
+    css: {
+      postcss: {
+        plugins: [tailwindcss(), autoprefixer],
+      },
     },
-  },
-} as UserConfig);
+  };
+});
