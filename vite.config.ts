ad07970924a2d5b137f10315988dcde53b3022c2
Jethro <ttppoo12144@gmail.com>
2024-07-19 22:21:01 +1000

    新增 環境變數

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,68 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "node:path";
+import { globSync } from "glob";
+import liveReload from "vite-plugin-live-reload";
+
+interface ServerOptions {
+  historyApiFallback?: boolean;
+}
+function moveOutputPlugin() {
+  return {
+    name: "move-output",
+    enforce: "post",
+    apply: "build",
+    async generateBundle(options, bundle) {
+      for (const fileName in bundle) {
+        if (fileName.startsWith("src/pages/")) {
+          const newFileName = fileName.slice("src/pages/".length);
+          bundle[fileName].fileName = newFileName;
+        }
+      }
+    },
+  };
+}
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/week3-4/" : "/",
+  base: import.meta.env.VITE_BASE_URL || "/",
   build: {
-    outDir: "dist",
     rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name].[hash].[ext]",
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        ...Object.fromEntries(
+          globSync("src/pages/**/*.html").map((file) => [
+            path.relative(
+              "src/pages",
+              file.slice(0, file.length - path.extname(file).length),
+            ),
+            file,
+          ]),
+        ),
       },
     },
+    outDir: "dist",
   },
+  server: {
+    historyApiFallback: true,
+  } as ServerOptions,
   plugins: [
-    ViteEjsPlugin(),
     tsconfigPaths(),
-    // Without Data
-    ViteEjsPlugin(),
-    // With Data
+    liveReload([
+      "./src/layout/**/*.ejs",
+      "./src/pages/**/*.ejs",
+      "./src/pages/**/*.html",
+    ]),
     ViteEjsPlugin({
-      domain: "example.com",
-      title: "My vue project!",
+      base: import.meta.env.VITE_BASE_URL || "/",
     }),
-    // Or With Vite Config
-    ViteEjsPlugin((viteConfig) => {
-      // viteConfig is the current viteResolved config.
-      return {
-        root: viteConfig.root,
-        domain: "example.com",
-        title: "My vue project!",
-      };
-    }),
+    moveOutputPlugin(),
   ],
   css: {
     postcss: {
       plugins: [tailwindcss(), autoprefixer],
     },
   },
-});
+} as UserConfig);
